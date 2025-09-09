cf9856c6ec198ced83af6276b7419f801a411a8a
Arath90 <ar90we17@gmail.com>
2025-09-09 16:37:05 -0700

    Front end para instrumentos echo

--- investment-strategy-app/frontend/src/main.jsx
+++ investment-strategy-app/frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Instrument from './components/InstrumentList.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Instrument />
+  </StrictMode>,
+)
+
